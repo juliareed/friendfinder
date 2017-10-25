014d09231cd393ca0f2abbc69fbc288ab325f0a0
Julia Reed <msjuliareed@gmail.com>
2017-10-25 12:25:15 -0500

    changed image size

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,190 @@
-module.exports = [];
+module.exports = [
+{
+	"name": "Crygonal",
+	"image": "https://cdn.bulbagarden.net/upload/thumb/1/11/615Cryogonal.png/250px-615Cryogonal.png",
+	"responses": [
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1",
+	"1"
+	],
+	"total": "10"
+},
+{
+	"name": "Litwick",
+	"image": "https://img.pokemondb.net/artwork/litwick.jpg",
+	"responses": [
+	"4",
+	"4",
+	"4",
+	"4",
+	"4",
+	"4",
+	"4",
+	"4",
+	"4",
+	"4"
+	],
+	"total": "40"
+},
+{
+	"name": "Lampent",
+	"image": "https://cdn.bulbagarden.net/upload/thumb/a/a5/608Lampent.png/250px-608Lampent.png",
+	"responses": [
+	"3",
+	"3",
+	"3",
+	"3",
+	"3",
+	"3",
+	"3",
+	"3",
+	"3",
+	"3"
+	],
+	"total": "30"
+},
+{
+	"name": "Chandelure",
+	"image": "https://cdn.bulbagarden.net/upload/thumb/6/65/609Chandelure.png/250px-609Chandelure.png",
+	"responses": [
+	"2",
+	"2",
+	"2",
+	"2",
+	"2",
+	"2",
+	"2",
+	"2",
+	"2",
+	"2"
+	],
+	"total": "20"
+},
+{
+	"name": "Klink",
+	"image": "https://cdn.bulbagarden.net/upload/thumb/e/ea/599Klink.png/250px-599Klink.png",
+	"responses": [
+	"2",
+	"2",
+	"3",
+	"3",
+	"4",
+	"4",
+	"5",
+	"5",
+	"1",
+	"1"
+	],
+	"total": "30"
+},
+{
+	"name": "Vanillite",
+	"image": "https://cdn.bulbagarden.net/upload/thumb/3/3f/582Vanillite.png/250px-582Vanillite.png",
+	"responses": [
+	"1",
+	"2",
+	"3",
+	"4",
+	"4",
+	"3",
+	"3",
+	"1",
+	"1",
+	"5"
+	],
+	"total": "27"
+},
+{
+	"name": "Buneary",
+	"image": "https://cdn.bulbagarden.net/upload/thumb/a/a7/427Buneary.png/250px-427Buneary.png",
+	"responses": [
+	"1",
+	"5",
+	"3",
+	"2",
+	"1",
+	"3",
+	"4",
+	"1",
+	"1",
+	"5"
+	],
+	"total": "26"
+},
+{
+	"name": "Drifloon",
+	"image": "https://cdn.bulbagarden.net/upload/thumb/e/eb/425Drifloon.png/250px-425Drifloon.png",
+	"responses": [
+	"4",
+	"2",
+	"2",
+	"3",
+	"5",
+	"1",
+	"1",
+	"3",
+	"2",
+	"1"
+	],
+	"total": "24"
+},
+{
+	"name": "Dugtrio",
+	"image": "https://cdn.bulbagarden.net/upload/thumb/e/e5/051Dugtrio.png/250px-051Dugtrio.png",
+	"responses": [
+	"1",
+	"1",
+	"1",
+	"5",
+	"5",
+	"3",
+	"2",
+	"2",
+	"1",
+	"1"
+	],
+	"total": "22"
+},
+{
+	"name": "Garbodor",
+	"image": "https://cdn.bulbagarden.net/upload/thumb/c/c4/569Garbodor.png/250px-569Garbodor.png",
+	"responses": [
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5",
+	"5"
+	],
+	"total": "50"
+},
+{
+	"name": "Girafarig",
+	"image": "https://cdn.bulbagarden.net/upload/thumb/1/11/203Girafarig.png/250px-203Girafarig.png",
+	"responses": [
+	"3",
+	"2",
+	"4",
+	"1",
+	"1",
+	"5",
+	"1",
+	"2",
+	"2",
+	"1"
+	],
+	"total": "22"
+}
+
+];
